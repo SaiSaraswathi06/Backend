19f9b3f79f0dcdf0ccb3d7a85ff916af1ff97a31
SaiSaraswathi06 <23A91A0594@aec.edu.in>
2025-12-30 13:57:35 +0530

    Put method

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -2,10 +2,8 @@ import student from "../models/studentsModels.js";
 
 const getStudentsDetails = async(req, res) => {
     try{
-            // const mydata = await student.find();
-            // const mydata = await student.findOne();
-            const mydata = await student.findById({_id: "694b80caf0cee6258678668e"})
-            res.status(200).json(mydata);
+        const mydata = await student.find();
+        res.status(200).json(mydata);
     }catch(error){
         console.log(error);
         res.status(500).json({error: error.message})
@@ -24,4 +22,54 @@ const addStudents = async(req, res) => {
         res.status(500).json({error: error.message})
     }
 };
-export {getStudentsDetails, addStudents};
+const getStudentById = async (req, res) => {
+    try{
+        const id = req.params.userid;
+        console.log("id :", id)
+        const data = await student.findById({_id: id});
+        console.log(data);
+        res.status(200).json(data);
+    }catch(error){
+        res.status(500).json({error: error.message})
+    }
+};
+// This is for testing 👀⁉️
+const getStudentsDetailsWithFilters = async(req, res) => {
+    try{
+        const {stdBranch, stdCollege, stdCgpa} = req.query;
+        console.log("stdBranch :", stdBranch);
+        console.log("stdCollege :", stdCollege);
+        console.log("stdCgpa :", stdCgpa);
+        res.status(200).json("success");
+    }catch(error){
+        res.status(500).json({error: error.message})
+    }
+
+}
+const updateStudents = async(req, res) => {
+    try{
+        const {id} = req.params;
+        const data = req.body;
+        // const updatedData = await student.findOneAndUpdate({stdRoll:id},data);
+        const updatedData = await student.findByIdAndUpdate({_id: id}, data, {new:true})
+        if(!updatedData){
+            return res.status(404).json({message: "user not existed"});
+        }
+        res.status(200).json({student: updatedData, message: "student Updated"})
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+
+}
+const updateStudentsStatus = async (req, res) => {
+    try{
+        await student.updateMany(
+            {status: false},
+            {$set: {status: true}}
+        );
+        res.status(200).json({message: "status updated"});
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+}
+export {getStudentsDetails, addStudents, getStudentById, getStudentsDetailsWithFilters, updateStudents, updateStudentsStatus};
